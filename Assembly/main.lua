a48142846e1a91ca2f38f9e8b1942f682d6c0af8
Deyan Dobromirov <dvd_video@abv.bg>
2017-01-16 12:48:25 +0200

    One line handler on export category

--- Assembly/main.lua
+++ Assembly/main.lua
@@ -26,10 +26,10 @@ asmlib.CreateTable("PIECES",{
 
 local defTable = asmlib.GetOpVar("DEFTABLE_PIECES")
  
-asmlib.DefaultType("Test",[[function(m)
+asmlib.DefaultType("Test",[[function(m)]]) --[===[
     local r = m:gsub("models/bobsters_trains/rails/2ft/",""):gsub("_","/")
     local s = r:find("/"); r = (s and r:sub(1,s-1) or "other");
-          r = r:gsub("^%l", string.upper); return r end]])
+          r = r:gsub("^%l", string.upper); return r end]]) ]===]
 
 asmlib.DefaultType("Bobster's two feet rails",[[function(m)
   local r = m:gsub("models/bobsters_trains/rails/2ft/",""):gsub("_","/")
@@ -41,12 +41,15 @@ function expCaegoty(sNam, vEq)
   local nEq = tonumber(vEq) or 0
   if(nEq <= 0) then
     return asmlib.StatusLog(nil, "Wrong equality <"..tostring(vEq)..">") end
-  local sEq  = ("="):rep(nEq)
+  local sEq, nLen = ("="):rep(nEq), (nEq+2)
   local tCat = asmlib.GetOpVar("TABLE_CATEGORIES")
   local ioF  = fileOpen(sNam, "w")
   for cat, rec in pairs(tCat) do
     if(asmlib.IsString(rec.Txt)) then
-      ioF:write("["..sEq.."["..cat..sEq..rec.Txt.."]"..sEq.."]\n")
+      local exp = "["..sEq.."["..cat..sEq..rec.Txt.."]"..sEq.."]"
+      if(not rec.Txt:find("\n")) then
+        return asmlib.StatusLog(nil, "Category one-liner <"..cat..">") end
+      ioF:write(exp.."\n")
     end
   end; ioF:flush(); ioF:close()
 end
@@ -66,17 +69,20 @@ function impCategory(sNam, vEq)
       if(sLin:sub(-1,-1) == "\r") then
         sLin = sLin:sub(1,-2) end
       local sFr, sBk = sLin:sub(1,nLen), sLin:sub(-nLen,-1)
-      if(sFr == cFr and not isPar) then
+      if(sFr == cFr and sBk == cBk) then
+        return asmlib.StatusLog(nil, "Category one-liner <"..sLin..">")
+      elseif(sFr == cFr and not isPar) then
         sPar, isPar = sLin:sub(nLen+1,-1).."\n", true
       elseif(sBk == cBk and isPar) then
         sPar, isPar = sPar..sLin:sub(1,-nLen-1), false
         local tBoo = stringExplode(sEq,sPar)
         local key, txt = tBoo[1], tBoo[2]
-        tCat[key] = {};
-        tCat[key].Txt = txt
+        if(key == "") then
+          return asmlib.StatusLog(nil, "Name missing <"..txt..">") end
+        if(not txt:find("function")) then
+          return asmlib.StatusLog(nil, "Function missing <"..key..">") end
+        tCat[key] = {}; tCat[key].Txt = txt
         tCat[key].Cmp = CompileString("return ("..txt..")",key)
-      elseif(sFr == cFr and sBk == cBk) then
-        return asmlib.StatusLog(nil, "Category inconsistent for <"..sLin..">")
       else
         sPar = sPar..sLin.."\n"
       end; sLin = ""
@@ -88,10 +94,10 @@ end
 
 expCaegoty("E:/Documents/Lua-Projs/ZeroBraineIDE/myprograms/ZeroBraineProjects/Assembly/cat.txt", 4)
 
-asmlib.SetOpVar("TABLE_CATEGORIES",{})
+--asmlib.SetOpVar("TABLE_CATEGORIES",{})
 
-logTable(impCategory("E:/Documents/Lua-Projs/ZeroBraineIDE/myprograms/ZeroBraineProjects/Assembly/cat.txt", 4),"tCat")
+--logTable(impCategory("E:/Documents/Lua-Projs/ZeroBraineIDE/myprograms/ZeroBraineProjects/Assembly/cat.txt", 4),"tCat")
 
-expCaegoty("E:/Documents/Lua-Projs/ZeroBraineIDE/myprograms/ZeroBraineProjects/Assembly/cat2.txt", 4)
+--expCaegoty("E:/Documents/Lua-Projs/ZeroBraineIDE/myprograms/ZeroBraineProjects/Assembly/cat2.txt", 4)
 
 
