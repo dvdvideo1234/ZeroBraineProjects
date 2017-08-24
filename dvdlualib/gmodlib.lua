d12c63b45495a0b4e8e8185546fb99f5b8074126
Deyan Dobromirov <dvd_video@abv.bg>
2017-08-24 17:52:09 +0300

    Added: Ray tracing intersection

--- dvdlualib/gmodlib.lua
+++ dvdlualib/gmodlib.lua
@@ -4,14 +4,62 @@ function tobool(any)
 end
 
 local mtVector = {__type = "Vector"}
-      mtVector.__tostring = function(self) return (self.x..","..self.y..","..self.z) end
+      mtVector.__tostring = function(self) return ("["..self.x..","..self.y..","..self.z.."]") end
+local function isVector(a) return (getmetatable(a) == mtVector) end
 function Vector(x,y,z)
   local self = {}; setmetatable(self, mtVector)
   self.x, self.y, self.z = tonumber(x) or 0, tonumber(y) or 0, tonumber(z) or 0
   function self:Mul(n) self.x, self.y, self.z = n*self.x, n*self.y, n*self.z end
   function self:Length() return math.sqrt(self.x^2 + self.y^2 + self.z^2) end
+  function self:Dot(v) return (self.x*v.x+self.y*v.y+self.z*v.z) end
+  function self:Add(v) self.x, self.y, self.z = (self.x+v.x), (self.y+v.y), (self.z+v.z); return self end
+  function self:Sub(v) self.x, self.y, self.z = (self.x-v.x), (self.y-v.y), (self.z-v.z); return self end
+  function self:Set(x,y,z)
+    if(getmetatable(x) == mtVector) then self.x, self.y, self.z = x.x, x.y, x.z;
+    else self.x, self.y, self.z = tonumber(x) or 0, tonumber(y) or 0, tonumber(z) or 0; end
+  end
+  function self:Cross(v)
+    local x = self.y * v.z - self.z * v.y
+    local y = self.z * v.x - self.x * v.z
+    local z = self.x * v.y - self.y * v.x
+    return Vector(x,y,z) -- self.x, self.y, self.z = x, y, z; return self
+  end
+  function self:Normalize()
+    local m = self:Length()
+    if(m ~= 0) then self.x, self.y, self.z = self.x/m, self.y/m, self.z/m end
+  end
+  function self:GetNormalized()
+    local m = self:Length()
+    if(m ~= 0) then return Vector(self.x/m, self.y/m, self.z/m) end; return Vector()
+  end
   return self
 end
+
+mtVector.__add = function(o1,o2)
+  local v1 = isVector(o1) and o1 or Vector(o1)
+  local v2 = isVector(o2) and o2 or Vector(o2)
+  local ov = Vector(); ov:Set(v1); ov:Add(v2); return ov 
+end
+
+mtVector.__sub = function(o1,o2)
+  local v1 = isVector(o1) and o1 or Vector(o1)
+  local v2 = isVector(o2) and o2 or Vector(o2)
+  local ov = Vector(); ov:Set(v1); ov:Sub(v2); return ov 
+end
+
+mtVector.__mul = function(o1,o2)
+  local v1 = isVector(o1) and o1
+  local v2 = isVector(o2) and o2
+  local ov = Vector()
+  if(v1 and v2) then ov:Set(v1); ov:Dot(v2); return ov end
+  if(v1 and tonumber(o2)) then ov:Set(v1); ov:Mul(tonumber(o2)); return ov end
+  if(v2 and tonumber(o1)) then ov:Set(v2); ov:Mul(tonumber(o1)); return ov end
+  print("Cannot preform multiplication between <"..type(o1).."/"..type(o2)..">")
+  return ov 
+end
+
+
+
 
 function string.Explode(d, s)
   return strExplode(s, d)
