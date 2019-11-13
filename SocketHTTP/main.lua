75ab5dc0a804f00ab77a81feb7b9aad2d5969f3d
dvdvideo1234 <dvd_video@abv.bg>
2019-11-13 11:41:51 +0200

    Added: Batch processor list

--- SocketHTTP/main.lua
+++ SocketHTTP/main.lua
@@ -3,16 +3,16 @@ local common = require("common")
 -- http://w3.impa.br/~diego/software/luasocket/http.html
 local https = require("ssl.https")
 local tBOM  = {"ef","bb","bf"}
+local fURL = "https://www.rulit.me/books/%s-%s-%s-%d.html"
+local sPth = "E:/Documents/Lua-Projs/ZeroBraineIDE/ZeroBraineProjects/SocketHTTP/%s.txt"
 
 -- https://www.rulit.me/books/tajnata-read-182168-1.html
--- char
-local iPT  = 3 -- Pages count. Book total pages to be scanned.
-local iLT  = 120 -- Symbols per line. Defines how long a line is
-local sID  = "182168" -- Book ID in the database
-local sNM  = "tajnata" -- Book name to complete the URL
-local fURL = "https://www.rulit.me/books/%s-read-%s-%d.html"
-local sPth = "E:/Documents/Lua-Projs/ZeroBraineIDE/ZeroBraineProjects/SocketHTTP/%s.txt"
-local function concat(oF, sBody)
+local tList =
+{
+  {Name = "tajnata", Mode= "read", ID = "182168", Size = 3, Wrap = 120}
+}
+
+local function concat(oF, sBody, iWrp)
   local nES, nEE, sO =  nil, nil, ""
   local nSS, nSE = sBody:find("<p>", 1, true)
   while(nSS and nSE) do
@@ -22,8 +22,8 @@ local function concat(oF, sBody)
       if(not sData:find("%w+=\"%w*\"")) then
         local sL = sBody:sub(nSE+1, nES-1)
         local iL = sL:len()
-        while(iL > iLT) do
-          local iK = iLT
+        while(iL > iWrp) do
+          local iK = iWrp
           local cH = sL:sub(iK, iK)
           while(cH ~= "") do
             if(cH:byte() == 32) then break end
@@ -41,16 +41,17 @@ local function concat(oF, sBody)
   end
 end
 
-local oF, sE = io.open(sPth:format(sNM), "wb")
-if(oF) then
-  for iD = 1, #tBOM do oF:write(string.char(tonumber(tBOM[iD], 16))) end
-  for iP = 1, iPT do
-    concat(oF, https.request(fURL:format(sNM, sID, iP)))
-  end; oF:flush(); oF:close()
-else
-  print(sE)
+for iB = 1, #tList do local vList = tList[iB]
+  local oF, sE = io.open(sPth:format(tostring(vList.Name or "default")), "wb")
+  if(oF) then
+    for iD = 1, #tBOM do oF:write(string.char(tonumber(tBOM[iD], 16))) end
+    for iP = 1, tonumber(vList.Size or 1) do
+      concat(oF, https.request(fURL:format(vList.Name, vList.Mode, vList.ID, iP)), (tonumber(vList.Wrap) or 80))
+    end; oF:flush(); oF:close()
+  else
+    print(sE)
+  end
 end
-
 
 
 
