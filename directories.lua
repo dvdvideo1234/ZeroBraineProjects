9bddcef904776ebcaf3dd086218571b876e9952e
dvdvideo1234 <dvd_video@abv.bg>
2020-05-28 18:38:51 +0300

    Added: Metadata manipulation

--- directories.lua
+++ directories.lua
@@ -1,40 +1,78 @@
 -- The location of the ZeroBraneStudio executable `zbstudio`
-local sInstall = "D:/LuaIDE"
-
-local tDir =
-{
-  "myprograms",
-  "ZeroBraineProjects",
-  "ZeroBraineProjects/dvdlualib",
-  -- When not located in general directory search in projects
-  "ZeroBraineProjects/ExtractWireWiki",
-}
 
 local metaDirectories =
 {
-  Base  = sInstall,
-  Count = 0
+  iBase = 0, -- The ID of the current IDE installation
+  tBase =
+  {
+    "D:/LuaIDE", -- Home machine
+    "C:/Users/ddobromirov/Documents/Lua-Projs/ZeroBraineIDE"
+  },
+  tPath =
+  {
+    "myprograms",
+    "ZeroBraineProjects",
+    "CorporateProjects",
+    "ZeroBraineProjects/dvdlualib",
+    -- When not located in general directory search in projects
+    "ZeroBraineProjects/ExtractWireWiki",
+  }
 }
 
-for iD = 1, #tDir do
-  local sP = tostring(tDir[iD] or "")
-  if(sP:len() > 0) then
-    local sD = sInstall.."/"..sP
-    local bS, sE, nE = os.execute("cd "..sD)
-    if(bS and bS ~= nil and nE == 0) then
-      metaDirectories.Count = metaDirectories.Count + 1
-      metaDirectories[metaDirectories.Count] = (sInstall.."/"..sP)
-      package.path = package.path..";"..sInstall.."/"..sP.."/?.lua"
-    else
-      print("Provided directory has been skipped: ["..sP.."]")
-    end
-  end
+local function tableClear(tT)
+  if(not tT) then error("Table missing") end
+  local sT = type(tT); if(sT ~= "table") then
+    error("Type mismatch ["..sT.."]") end
+  if(not next(tT)) then return tT end
+  for k, v in pairs(tT) do tT[k] = nil end
 end
 
 local directories = {}
 
+function directories.getCount()
+  return metaDirectories.iCount
+end
+
+function directories.getTPath()
+  return metaDirectories.tPath
+end
+
+function directories.addTPath(...)
+  local tPath = metaDirectories.tPath
+  local tA, iK = {...}, 1
+  while(tA[iK]) do
+    local sV = tostring(tA[iK] or "")
+    table.insert(metaDirectories.tPath, sV)
+    iK = iK + 1
+  end
+end
+
+function directories.setTPath(...)
+  tableClear(metaDirectories.tPath)
+  directories.addTPath(...)
+end
+
+function directories.getTBase()
+  return metaDirectories.tBase
+end
+
+function directories.addTBase(...)
+  local tBase = metaDirectories.tBase
+  local tA, iK = {...}, 1
+  while(tA[iK]) do
+    local sV = tostring(tA[iK] or "")
+    table.insert(metaDirectories.tBase, sV)
+    iK = iK + 1
+  end
+end
+
+function directories.setTBase(...)
+  tableClear(metaDirectories.tBase)
+  directories.addTBase(...)
+end
+
 function directories.getBase()
-  return metaDirectories.Base
+  return metaDirectories.sBase, metaDirectories.iBase
 end
 
 function directories.getLast()
@@ -46,22 +84,41 @@ function directories.getFirst()
 end
 
 function directories.getByID(vD)
-  local iD = (tonumber(vD) or 0)
-  if(iD < 1) then error("Identifier mismatch ["..iD.."]") end
-  local sP = metaDirectories[iD]
-  if(not sP) then error("Missing path under id ["..iD.."]") end
+  local iD = (tonumber(vD) or 0); if(iD < 1) then
+    error("Identifier mismatch ["..iD.."]") end
+  local sP = metaDirectories[iD]; if(not sP) then
+    error("Missing path under ID ["..iD.."]") end
   return sP
 end
 
-function directories.getList()
-  local tL, iD = {}, 1
-  while(metaDirectories[iD]) do
-    tL[iD] = metaDirectories[iD]
-    iD = iD + 1
+function directories.setBase(vB)
+  local iBase = math.floor(tonumber(vB) or 0)
+  local tBase = metaDirectories.tBase
+  if(not (tBase and next(tBase))) then 
+    error("Directory base list missing") end
+  local sBase = tostring(tBase[iBase] or "")
+  if(sBase:len() <= 0) then 
+    error("Directory base path missing ["..iBase.."]") end
+  local iCount = 0 -- Stores the number of paths processed
+  local tPath = metaDirectories.tPath
+  metaDirectories.iCount = iCount
+  metaDirectories.iBase  = iBase  
+  metaDirectories.sBase  = sBase  
+  for iD = 1, #tPath do
+    local sP = tostring(tPath[iD] or "")
+    if(sP:len() > 0) then
+      local sD = (sBase.."/"..sP)
+      local bS, sE, nE = os.execute("cd /d "..sD)
+      if(bS and bS ~= nil and nE == 0) then
+        iCount = iCount + 1; metaDirectories.iCount = iCount
+        metaDirectories[iCount] = sD
+        package.path = package.path..";"..sD.."/?.lua"
+      else
+        print("Provided directory has been skipped: ["..sD.."]")
+      end
+    end
   end
-  tL.Base  = metaDirectories.Base
-  tL.Count = (iD - 1)
-  return tL
+  return directories
 end
 
 return directories
