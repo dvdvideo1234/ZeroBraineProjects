6162ed84e9778e55adcf6085e65119d15e9fee43
dvdvideo1234 <dvd_video@abv.bg>
2023-01-21 18:14:04 +0200

    Updated: DSliderButton

--- GmodPanels/dsliderbutton.lua
+++ GmodPanels/dsliderbutton.lua
@@ -6,14 +6,21 @@ function PANEL:Init()
   self.SSX, self.SSY = 0, 22
   self.PBX, self.PBY = 0, 0
   self.SBX, self.SBY = 0, 22
-  self.Slider = vgui.Create("DNumSlider", self)
-  self.Slider:Dock(TOP)
-  self.Slider:SetDark(true)
-  self.Slider:SetHeight(self.SY)
+end
+
+function PANEL:SetDelta(nX, nY)
+  local eX = (tonumber(nX) or 2)
+  if(eX >= 0) then self.EDX = eX end
+  local eY = (tonumber(nN) or 2)
+  if(eY >= 0) then self.EDY = eY end
+  return self
 end
 
 -- https://github.com/Facepunch/garrysmod/blob/master/garrysmod/lua/vgui/dnumslider.lua
-function PANEL:Slider(sVar, sNam, sTyp)
+function PANEL:SetSlider(sVar, sNam, sTyp)
+  self.Slider = vgui.Create("DNumSlider", self)
+  self.Slider:Dock(TOP)
+  self.Slider:SetTall(self.SSY)
   self.Slider:SetText(sNam)
   self.Slider:SetConVar(sVar)
   self.Slider:SizeToContents()
@@ -21,16 +28,27 @@ function PANEL:Slider(sVar, sNam, sTyp)
   self:AddItem(self.Slider)
 end
 
-function PANEL:Limits(nMin, nMax, nDef, iDig)
-  self.Slider:SetMinMax(nMin, nMax)  
-  if(iDig != nil) then self.Slider:SetDecimals(iDig) end
-  if(nDef != nil) then self.Slider:SetDefaultValue(nDef) end
-  self.Slider:UpdateNotches()
+function PANEL:GetSlider()
+  return self.Slider
 end
 
-function PANEL:SetTall(nS, nB)
+function PANEL:Configure(nMin, nMax, nDef, iDig)
+  self.Slider:SetMin(tonumber(nMin) or -10)
+  self.Slider:SetMax(tonumber(nMax) or  10)
+  if(iDig != nil) then self.Slider:SetDecimals(iDig) end
+  if(nDef != nil) then self.Slider:SetDefaultValue(nDef) end
+  self.Slider:UpdateNotches(); return self
+end
+
+function PANEL:SetTall(nP, nS, nB)
+  if(nP) then self.SPY = math.floor(nP) end
   if(nS) then self.SSY = math.floor(nS) end
   if(nB) then self.SBY = math.floor(nB) end
+  return self
+end
+
+function PANEL:GetTall()
+  return self.SPY, self.SSY, self.SBY
 end
 
 function PANEL:SetWide(nW)
@@ -49,36 +67,54 @@ function PANEL:SetWide(nW)
       tBut[iD]:SetSize(self.SBX, self.SBY)
       self.PBX = self.PBX + self.SBX + self.EDX
     end
-  end
+  end; return self
 end
 
+function PANEL:GetWide()
+  return self.SPX, self.SSX, self.SBX
+end  
+
 -- https://github.com/Facepunch/garrysmod/blob/master/garrysmod/lua/vgui/dbutton.lua
-function PANEL:Button(fAct, sNam, sTyp)
+function PANEL:SetButton(fAct, sNam, sTyp)
   local pBut = vgui.Create("DButton", self)
   local tBut = self.Button
   if(not tBut) then tBut = {Size = 0} else
     if(not tBut.Size) then tBut.Size = 0 end
-    if(tBut.Size < 0) then table.Clear(tBut); tBut.Size = 0 end
+    if(tBut.Size < 0) then table.Empty(tBut); tBut.Size = 0 end
   end
-  local iSiz = (self.Button.Size + 1); tBut[iSiz] = pBut
+  local iSiz = (self.Button.Size + 1); table.insert(tBut, pBut)   tBut[iSiz] = 
   pBut:SetText(sNam); if(sTyp ~= nil) then pBut:SetTooltip(sTyp) end
   pBut.DoClick = function()
     local pS, sE = pcall(fAct, pBut, self.Slider)
-    if(not pS) then error("Button("..pBut:GetText().."): "..sE)
+    if(not pS) then error("["..pBut:GetText().."]: "..sE)
   end
   pBut.DoRightClick = function()
     SetClipboardText(pBut:GetText())
   end
   self.Button = tBut
   self.Button.Size = iSiz
-  return pBut
+  return self
 end
 
-function PANEL:GetButtonID(vD)
+function PANEL:GetButton(vD)
   local iD = math.floor(tonumber(vD) or 0)
   if(iD <= 0) then return nil end
-  if(not self.Button) then return nil end
-  return self.Button[iD]
+  local tBut = self.Button
+  if(not tBut) then return nil end
+  return tBut[iD]
+end
+
+function PANEL:RemoveButton(vD)
+  local iD = math.floor(tonumber(vD) or 0)
+  if(iD < 0) then return self end
+  local tBut = self.Button
+  if(not tBut) then return self end
+  local iB = ((iD > 0) and iD or nil)
+  local pBut = table.remove(tBut, iB)
+  if(pBut) then
+    pBut:Remove()
+    tBut.Size = tBut.Size = 1
+  end; return self
 end
 
 function PANEL:UpdateColours(tSkin)
@@ -91,4 +127,4 @@ function PANEL:UpdateColours(tSkin)
   end
 end
 
-derma.DefineControl("DSliderButton", "Button interactive slider", PANEL, "DSizeToContents")
+derma.DefineControl("DSliderButton", "Button-interactive slider", PANEL, "DSizeToContents")
