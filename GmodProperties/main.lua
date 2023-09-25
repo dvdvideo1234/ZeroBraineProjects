a825656e5ef9d1533dc7c02273f68ca31f48b3a2
Deyan Dobromirov <dvd_video@abv.bg>
2023-09-25 16:12:18 +0300

    Added: Property files checker for Gmod

---
+++ GmodProperties/main.lua
@@ -0,0 +1,41 @@
+local drpath = require("directories")
+      drpath.addPath("myprograms",
+                  "ZeroBraineProjects",
+                  "CorporateProjects",
+                  -- When not located in general directory search in projects
+                  "ZeroBraineProjects/dvdlualib",
+                  "ZeroBraineProjects/ExtractWireWiki")
+      drpath.addBase("D:/Programs/LuaIDE")
+      drpath.addBase("C:/Programs/ZeroBraineIDE").setBase(2)
+local f
+require("turtle")
+require("gmodlib")
+local com = require("common")
+local cpx = require("complex")
+
+local source = "laseremitter.properties"
+
+if(source:find("/") or source:find("\\")) then
+  local name = source:gsub("\\", "/"):gsub("/+", "/")
+  f = assert(io.open(source, "r"))
+else
+  local name = com.stringGetChunkPath().."Data\\"..source
+        name = name:gsub("\\", "/"):gsub("/+", "/")
+  f = assert(io.open(name, "r"))
+end
+
+local r, t = f:read("*line"), {}
+while(r) do
+  local n = com.stringTrim(r)
+  if(n:len() > 0 and n:sub(1,1) ~= "#") then
+    local s, e = n:find("=", 1, true)
+    if(not s) then error("Equals sign missing ["..r.."]") end
+    if(s ~= e) then error("Equals sign is longer at ["..r.."]") end
+    local key, dat = n:sub(1,s-1), n:sub(e+1,-1)
+    if(t[key]) then error("Key exists at ["..r.."]") end
+    t[key] = true; print(key, dat)
+  end
+  r = f:read("*line")
+end
+
+f:close()
