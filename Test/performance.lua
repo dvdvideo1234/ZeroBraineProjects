e0b9d1f5a60968416572ec3f2dd75b8b6fc4c04b
Deyan Dobromirov <dvd_video@abv.bg>
2024-09-18 09:06:46 +0300

    Added: Wrap performance

--- Test/performance.lua
+++ Test/performance.lua
@@ -11,28 +11,53 @@ local drpath = require("directories")
 require("dvdlualib/gmodlib")
 local testexec = require("testexec")
 
-function GetEmpty1(sBas, fEmp, iCnt, ...)
-  local tV = {...}
-  local iC = math.floor(tonumber(iCnt) or 0)
-  return iC
+local nF, nT = 1, 3
+
+function GetWrap(nV,nL,nH) local nV = nV
+  while(nV < nL or nV > nH) do
+    nV = ((nV < nL) and (nH - (nL - nV) + 1) or nV)
+    nV = ((nV > nH) and (nL + (nV - nH) - 1) or nV)
+  end; return nV -- Returns the N-stepped value
 end
 
-function GetEmpty2(sBas, fEmp, iCnt, ...)
-  local tV = {...}
-  local iC = select("#", ...)
-  return iC
+function GetWrap2(nV,nL,nH)
+  if(nV == 0) then return nH end
+  if(nV >= nL and nV <= nH) then return nV end
+  local nC = nV % nH
+  return (nC == 0) and nH or nC
 end
+
+
+function GetEmpty1(tArg)
+  return GetWrap(unpack(tArg))
+end
+
+function GetEmpty2(tArg)
+  return GetWrap2(unpack(tArg))
+end
+
 t = testexec.New()
 t:setCase(GetEmpty1, "original")
 t:setCase(GetEmpty2, "modified")
 t:setProgress(1, 0.1)
-t:setCount(12000, 12000)
-t:setCard({nil, nil, 3, 1, 2, 3}, 3, "0")
-t:setCard({nil, nil, 3, nil, 2, 3}, 3, "1")
-t:setCard({nil, nil, 3, 1, nil, 3}, 3, "2")
-t:setCard({nil, nil, 3, 1, 2, nil}, 3, "3")
-t:setCard({nil, nil, 3, nil, nil, 3}, 3, "1")
-t:setCard({nil, nil, 3, 1, nil, nil}, 3, "2")
-t:setCard({nil, nil, 3, nil, 2, nil}, 3, "3")
-t:setCard({nil, nil, 3, nil, nil, nil}, 3, "3")
+t:setCount(10000, 10000)
+t:setCard({-6, nF, nT}, 3, "1 ")
+t:setCard({-5, nF, nT}, 1, "2 ")
+t:setCard({-4, nF, nT}, 2, "3 ")
+t:setCard({-3, nF, nT}, 3, "4 ")
+t:setCard({-2, nF, nT}, 1, "5 ")
+t:setCard({-1, nF, nT}, 2, "6 ")
+t:setCard({0 , nF, nT}, 3, "7 ")
+t:setCard({1 , nF, nT}, 1, "8 ")
+t:setCard({2 , nF, nT}, 2, "9 ")
+t:setCard({3 , nF, nT}, 3, "10")
+t:setCard({4 , nF, nT}, 1, "11")
+t:setCard({5 , nF, nT}, 2, "12")
+t:setCard({6 , nF, nT}, 3, "13")
+t:setCard({7 , nF, nT}, 1, "14")
+t:setCard({8 , nF, nT}, 2, "15")
+t:setCard({9 , nF, nT}, 3, "16")
+t:setCard({10, nF, nT}, 1, "17")
 t:runMeasure()
+
+print(-7 % 5, 7 % 5)
