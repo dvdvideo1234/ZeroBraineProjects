f231f5c0a9d10547bb4daa85fc553611f509aca1
Deyan Dobromirov <dvd_video@abv.bg>
2025-10-06 14:33:34 +0300

    Added: Dynamic dispersion graph

--- Test/test_dispersion.lua
+++ Test/test_dispersion.lua
@@ -6,7 +6,7 @@ local drpath = require("directories")
                   "ZeroBraineProjects/dvdlualib",
                   "ZeroBraineProjects/ExtractWireWiki")
       drpath.addBase("D:/Programs/LuaIDE")
-      drpath.addBase("C:/Programs/ZeroBraineIDE").setBase(1)
+      drpath.addBase("C:/Programs/ZeroBraineIDE").setBase(2)
 
 require("turtle")
 require("gmodlib")
@@ -14,37 +14,52 @@ local com = require("common")
 local cpx = require("complex")
 local col = require("colormap")
 local crm = require("chartmap")
+local kym = require("keymap")
 
 -------------------------------------
 -- https://en.wikipedia.org/wiki/List_of_refractive_indices
 -- http://hyperphysics.phy-astr.gsu.edu/hbase/geoopt/dispersion.html#c1
-local step = 10 -- Fused qartz
 local LaserLib, DATA = {}, {}
 
-DATA.AMAX = {-360, 360}        -- General angular limits for having min/max
-DATA.WVIS = { 700, 300}        -- General wavelength limits for visible light
-DATA.WCOL = {  0 , 300}        -- Mapping for wavelength to color hue conversion
-DATA.WMAP = {  20, 5}        -- Dispersion wavelength mapping for refractive index
+DATA.WVIS = { 650, 310}        -- General wavelength limits for visible light
+DATA.WCOL = {  0 , 320}        -- Mapping for wavelength to color hue conversion
+DATA.WMAP = {  20,   5}        -- Dispersion wavelength mapping for refractive index
 DATA.SODD = 589.29             -- General wavelength for sodium line used for dispersion
-DATA.SOMR = 10                 -- General coefficient for wave to refractive index converion
+DATA.SOMR = 10                 -- General coefficient for wave to refractive index conversion
 
-local nIndx = 1.458
+local nIndx = 1.333            -- Water
+
+local function HSVToColor(h,s,v)
+  local r, g, b = col.getColorHSV(h,s,v)
+  return {r = r, g = g, b = b, a = 255}
+end
 
 -------------------------------------
 
+local greyLevel  = 200
 local dX, dY = 10, 0.01
 local W , H = 1000, 600
 local minX, maxX = DATA.WVIS[2], DATA.WVIS[1]
-local minY, maxY = 1.4, 1.6
-local greyLevel  = 200
-local intX  = crm.New("interval","WinX", minX, maxX, 0, W)
-local intY  = crm.New("interval","WinY", minY, maxY, H, 0)
-local clGry = colr(greyLevel,greyLevel,greyLevel)
+local minY, maxY = nIndx-0.1, nIndx+0.15
 local clB = colr(col.getColorBlueRGB())
 local clR = colr(col.getColorRedRGB())
 local clBlk = colr(col.getColorBlackRGB())
+local clGry = colr(greyLevel,greyLevel,greyLevel)
+
+local intX  = crm.New("interval","WinX", minX, maxX, 0, W)
+local intY  = crm.New("interval","WinY", minY, maxY, H, 0)
 local scOpe = crm.New("scope"):setInterval(intX, intY):setBorder(minX, maxX, minY, maxY)
       scOpe:setSize(W, H):setColor(clBlk, clGry):setDelta(dX, dY)
+local tMatch = 
+{
+  {Name = "Violet", S = 380, E = 450},
+  {Name = "Blue"  , S = 450, E = 485},
+  {Name = "Cyan"  , S = 485, E = 500},
+  {Name = "Green" , S = 500, E = 565},
+  {Name = "Yellow", S = 565, E = 590},
+  {Name = "Orange", S = 590, E = 625},
+  {Name = "Red"   , S = 625, E = 750}
+}
 
 -------------------------------------------------------
 
@@ -56,43 +71,98 @@ function LaserLib.WaveToIndex(wave, nidx)
   return (-math.log(x) / ms - h) + nidx
 end
 
-local tS, w = {}, DATA.WVIS[1] 
-
-for w = DATA.WVIS[2], DATA.WVIS[1] do
-  local c = cpx.getNew(w, LaserLib.WaveToIndex(w, nIndx))
-  if(w % 100 == 0) then
-    print(c, w)
-  end
-  --print(c)
-  table.insert(tS, c)
+function LaserLib.WaveToColor(wave, bobc)
+  local wvis, wcol = DATA.WVIS, DATA.WCOL
+  local hue = math.Remap(wave, wvis[1], wvis[2], wcol[1], wcol[2])
+  local tab = HSVToColor(hue, 1, 1) -- Returns table not color
+  if(bobc) then local wtcol = DATA.WTCOL
+    wtcol.r, wtcol.g = tab.r, tab.g
+    wtcol.b, wtcol.a = tab.b, tab.a; return wtcol
+  end; return tab.r, tab.g, tab.b, tab.a
 end
 
 -------------------------------------------------------
 
-if(tS) then
-  com.logStatus("The distance between every grey line on X is: "..tostring(dX))
-  com.logStatus("The distance between every grey line on Y is: "..tostring(dY))
-  
-  local function drawComplexLine(S, E, Cl)
-    local x1 = intX:Convert(S:getReal()):getValue()
-    local y1 = intY:Convert(S:getImag()):getValue()
-    local x2 = intX:Convert(E:getReal()):getValue()
-    local y2 = intY:Convert(E:getImag()):getValue()
-    pncl(Cl); line(x1, y1, x2, y2)
-  end; cpx.setAction("ab", drawComplexLine)
+com.logStatus("The distance between every grey line on X is: "..tostring(dX))
+com.logStatus("The distance between every grey line on Y is: "..tostring(dY))
 
-  open("Complex Bezier curve")
-  size(W,H); zero(0, 0); updt(false) -- disable auto updates
+local function drawComplexLine(S, E, Cl)
+  local x1 = intX:Convert(S:getReal()):getValue()
+  local y1 = intY:Convert(S:getImag()):getValue()
+  local x2 = intX:Convert(E:getReal()):getValue()
+  local y2 = intY:Convert(E:getImag()):getValue()
+  pncl(Cl); line(x1, y1, x2, y2)
+end; cpx.setAction("ab", drawComplexLine)
 
+open("Complex Bezier curve")
+
+local function drawGraph()
+  wipe()
   scOpe:Draw(false, false, true, true)
+  scOpe:setSizeVtx(5)
+  local o = cpx.getNew(0, 0.05)
+  local r, g, b = LaserLib.WaveToColor(DATA.SODD)
+  local idx = LaserLib.WaveToIndex(DATA.SODD, nIndx)
+  local c, tS = cpx.getNew(DATA.SODD, idx), {}
+  for w = DATA.WVIS[2], DATA.WVIS[1], 5 do
+    local c = cpx.getNew(w, LaserLib.WaveToIndex(w, nIndx))
+    table.insert(tS, {c = c, w = w})
+  end
+  scOpe:drawComplexPoint(c, colr(r, g, b))
+  scOpe:drawComplexText(c, tostring(c:getRound(0.01)).." : Sodium line", -90)
+  for iC = 1, #tMatch do
+    local v = tMatch[iC]
+    local r, g, b = LaserLib.WaveToColor(v.S)
+    local idx = LaserLib.WaveToIndex(v.S, nIndx)
+    local c = cpx.getNew(v.S, idx)
+    scOpe:drawComplexPoint(c, colr(r, g, b))
+    scOpe:drawComplexText(c, tostring(c:getRound(0.01)).." : "..v.Name, 90)
+  end
+  scOpe:setSizeVtx(2)
 
   for iD = 1, (#tS-1) do
-    tS[iD]:Action("ab", tS[iD+1], clR)
-    scOpe:drawComplexPoint(tS[iD])
-    updt(); wait(0.01)
+    local v1, v2 = tS[iD], tS[iD+1]
+    local r, g, b = LaserLib.WaveToColor(v1.w)
+    local ang, btx = -45, ((v1.w % 50) == 0)
+    v1.c:Action("ab", v2.c, colr(r, g, b))
+    scOpe:drawComplexPoint(v1.c, colr(r, g, b), btx, ang)
   end 
   
-  wait()
-else
-  common.logStatus("Your curve parameters are invalid !")
+  text(("DATA.WVIS = %6.2f : %6.2f"):format(DATA.WVIS[1], DATA.WVIS[2]), 0, 0, 0)
+  text(("DATA.WCOL = %6.2f : %6.2f"):format(DATA.WCOL[1], DATA.WCOL[2]), 0, 0, 15)
+  text(("DATA.WMAP = %6.2f : %6.2f"):format(DATA.WMAP[1], DATA.WMAP[2]), 0, 0, 30)
+  
+  updt()
 end
+
+function getKey(t, k, u, d, l, r)
+  local step = 5
+  if(kym.isKey(k, u)) then
+    t[1] = t[1] + step
+    drawGraph()
+  elseif(kym.isKey(k, d)) then
+    t[1] = t[1] - step
+    drawGraph()
+  elseif(kym.isKey(k, l)) then
+    t[2] = t[2] - step
+    drawGraph()
+  elseif(kym.isKey(k, r)) then
+    t[2] = t[2] + step
+    drawGraph()
+  end
+end
+
+size(W,H); zero(0, 0); updt(false) -- disable auto updates
+
+drawGraph()
+
+shift = 1
+
+while true do
+  local key = char(); wait(0.1)
+  getKey(DATA.WVIS, key, "K_UP", "K_DOWN", "K_LEFT", "K_RIGHT")
+  getKey(DATA.WCOL, key, "K_HOME", "K_END", "K_DELETE", "K_PAGEDOWN")
+  getKey(DATA.WMAP, key, "K_NUMPAD8", "K_NUMPAD5", "K_NUMPAD4", "K_NUMPAD6")
+end
+
+wait()
