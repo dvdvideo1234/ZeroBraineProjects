048d0fad7b0799ec34038db20c7d4ac83c877be1
Deyan Dobromirov <dvd_video@abv.bg>
2025-12-10 14:50:33 +0200

    Added: GPT translations

--- GmodLanguage/GPT/main.lua
+++ GmodLanguage/GPT/main.lua
@@ -8,19 +8,40 @@ local dir = require("directories")
       dir.addBase("D:/Programs/LuaIDE")
       dir.addBase("C:/Programs/ZeroBraineIDE").setBase(2)
 
-local common = require("common")
-local https = require("ssl.https")
-local sBase = "GmodLanguage/GPT"
+local chatbot = require("chatbot")
 
-local body, code, headers, status = https.request("https://chatgpt.com/?q=Help+me+pick+an+outfit+that+will+look+good+on+camera")
+local sBas = "GmodLanguage/GPT"
 
-local f = assert(io.open(sBase.."/body.html", "w"))
+local url = "https://api.groq.com/openai/v1/chat/completions"
+local prompt = "Translate the abbreviation to Bulgarian \"%s\". I need only the final result/conclusion."
+local abrev = "tool.test.reload=Reload the testing method"
 
-f:write(body)
-f:flush()
-f:close()
+local bot = chatbot.getNew()
+bot:JSON("dkjson")
+local rem = bot:Remote(url, "${GROQ_API_CHAT_KEY}")
+bot:Request({
+  model = "llama-3.1-8b-instant",  -- or "mixtral-8x7b-32768"
+  messages = {{role = "user", content = prompt:format(abrev)}},
+  temperature = 0.7,
+  max_tokens = 1024
+})
+local res = bot:Response({
+  protocol = "tlsv1_2",
+  options  = "all",
+  timeout  = 20
+})
 
-print(code, status)
+print(tostring(bot))
 
-common.logTable(headers, "headers")
+if(bot:getStatus() == 200) then
+  local body = bot:getBody(true)
+  if(body) then
+    print(body.choices[1].message.role)
+    print(body.choices[1].message.content)
+  else
+    bot:Dump()
+  end
+else
+  bot:Dump()
+end
 
