65f59e735526b30aa2140c0ce0ba9e52b77c428b
Deyan Dobromirov <dvd_video@abv.bg>
2026-01-05 12:26:23 +0200

    Updated: Call Chatbot inside a loop for each row

--- GmodLanguage/GPT/main.lua
+++ GmodLanguage/GPT/main.lua
@@ -9,39 +9,64 @@ local dir = require("directories")
       dir.addBase("C:/Programs/ZeroBraineIDE").setBase(2)
 
 local chatbot = require("chatbot")
+local common = require("common")
 
-local sBas = "GmodLanguage/GPT"
+local sBas = common.stringGetChunkPath()
 
-local url = "https://api.groq.com/openai/v1/chat/completions"
-local prompt = "Translate the abbreviation to Bulgarian \"%s\". I need only the final result/conclusion."
-local abrev = "tool.test.reload=Reload the testing method"
-
-local bot = chatbot.getNew()
-bot:JSON("dkjson")
-local rem = bot:Remote(url, "${GROQ_API_CHAT_KEY}")
-bot:Request({
-  model = "llama-3.1-8b-instant",  -- or "mixtral-8x7b-32768"
-  messages = {{role = "user", content = prompt:format(abrev)}},
-  temperature = 0.7,
-  max_tokens = 1024
-})
-local res = bot:Response({
-  protocol = "tlsv1_2",
-  options  = "all",
-  timeout  = 20
-})
-
-print(tostring(bot))
-
-if(bot:getStatus() == 200) then
-  local body = bot:getBody(true)
-  if(body) then
-    print(body.choices[1].message.role)
-    print(body.choices[1].message.content)
+local prompt = "You are an expert on Bulgarian philology. Translate to Bulgarian [%s]."
+      prompt = prompt.." I need only the final result/conclusion."
+      prompt = prompt.." Do not quote the result or add aditional spaces/punctoation."
+local bot = chatbot.getNew("Translate"):setJSON("dkjson"); print("Bot created:", tostring(bot))
+      bot:setKey("${GROQ_API_CHAT_KEY}")
+      bot:setAPI("https://api.groq.com/openai/v1/chat/completions")
+if(not bot:isValid()) then error("Bot is invalid!") end
+      
+local fin = assert(io.open(sBas.."in.properties", "rb"))
+local cur, fmt = fin:seek(), "%5.2f%%[O]" -- Get the current pointer
+local siz = fin:seek("end"); fin:seek("set", cur)
+local fou = assert(io.open(sBas.."out.properties", "wb"))
+local row, abr, lib, cnt = fin:read("*line"), "", "", 1000
+while(row and cnt > 0) do
+  cur = fin:seek() 
+  print(fmt:format(cur/siz*100), row)
+  local s, e = row:find("=", 1, true)
+  if(s and e) then
+    abr = row:sub(1, s-1)
+    lib = row:sub(e+1, -1)
+    bot:Request({
+      model = "llama-3.1-8b-instant",  -- or "mixtral-8x7b-32768"
+      messages = {{role = "user", content = prompt:format(lib)}},
+      temperature = 0.6,
+      max_tokens = 1024
+    })
+    local res = bot:Response({
+      protocol = "tlsv1_2",
+      options  = "all",
+      timeout  = 20
+    })
+    if(bot:getStatus() == 200) then
+      local body = bot:getBody(true)
+      if(body) then
+        lib = body.choices[1].message.content
+      else
+        bot:Dump()
+        break
+      end
+    else
+      bot:Dump()
+      break
+    end
+    print("      [T]", abr.."="..lib)
+    fou:write(abr.."="..lib.."\n")
   else
-    bot:Dump()
+    print("      [K]", row)
+    fou:write(row.."\n")
   end
-else
-  bot:Dump()
+  row = fin:read("*line")
+  common.timeDelay(1)
+  cnt = cnt - 1
 end
 
+fou:flush()
+fou:close()
+fin:close()
