743a7c752ae99aa2bc23636696340f102e8e1791
Deyan Dobromirov <dvd_video@abv.bg>
2026-09-09 09:26:42 +0300

    Updated: Testing scenario for removing the op-cars

--- Assembly/data/imp_exp.lua
+++ Assembly/data/imp_exp.lua
@@ -12,8 +12,7 @@ local com = require("common")
 local rev = "C:/Users/ddobromirov/Documents/Lua-Projs/VerControl/TrackAssemblyTool_GIT/lua/"
 local bas = dir.getBase().."/ZeroBraineProjects/"
 
-rawset(_G, "CLIENT", false)
-
+rawset(_G, "CLIENT", true)
 rawset(_G, "SERVER", (not CLIENT))
 require("gmodlib")
 
@@ -24,14 +23,10 @@ local function CongigureLIB(sRev)
   dofile(sRev.."trackassembly/trackasmlib.lua")
   local asmlib = trackasmlib 
   if not asmlib then error("No library") end
-  local SetOpVar = asmlib.SetOpVar
-  asmlib.SetOpVar = function(n, ...)
-    if (n ~= "DIRPATH_BAS") then
-      return SetOpVar(n, ...)
-    else  
-      return SetOpVar(n, bas.."Assembly/trackassembly/")
-    end
-  end
+  common.tableIndexProxy(asmlib,
+  function(i, k, v)
+    return ((k == "DIRPATH_BAS") and (bas.."Assembly/trackassembly/") or v)
+  end)
   local gnIndependentUsed = bit.bor(FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_PRINTABLEONLY)
   local NewAsmConvar = asmlib.NewAsmConvar
   CreateConVar("trackassembly_logsmax", 0, gnIndependentUsed, "Maximum logging lines being written before the counter is reset", 0, 100000)
@@ -49,7 +44,7 @@ end
 local asmlib = CongigureLIB(rev)
 
 asmlib.IsFlag("file_read_once", true)
-asmlib.SetOpVar("MODE_DATABASE", "LUA")
+asmlib.MODE_DATABASE = "LUA"
 asmlib.IsModel = function(m) return isstring(m) end
 
 -------- CUSTOM TEST --------
@@ -61,20 +56,18 @@ local sT = "Shinji85's Rails"
 
 local sP = asmlib.GetTypePrefix(sT)
 local sE, tC = sP, {}
-local sG = asmlib.GetOpVar("DBEXP_PREFGEN")
-local sM = asmlib.GetOpVar("MODE_DATABASE")
+local sG = asmlib.DBEXP_PREFGEN
+local sM = asmlib.MODE_DATABASE
 
 require(("Assembly/autorun/z_auto"..sS.."_[%s]"):format(sP))
 
 if(sS == "run") then
   local nT = os.clock()
   asmlib.ImportDSV("PIECES", true, sP)
-  --asmlib.ImportDSV1("ADDITIONS", true, sP)
-  --asmlib.ImportCategory(0, sP, false)
+  asmlib.ImportDSV("ADDITIONS", true, sP)
+  asmlib.ImportCategory(0, sP, false)
   print("Elapsed: "..((os.clock() - nT) * 1000).."ms")
 end
-
-if(true) then return end
 
 asmlib.WorkshopID(sP, tostring(0):rep(3))
 asmlib.WorkshopID(sT, tostring(0):rep(3))
